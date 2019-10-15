d42395bd32bf98b7156fa44d66d59cc6df790887
hatlonely <hatlonely@gmail.com>
2019-10-16 02:41:29 +0800

    update

--- src/components/Articles.vue
+++ src/components/Articles.vue
@@ -9,7 +9,7 @@
         <v-layout align-center justify-center fill-height text-center row wrap>
           <template v-for="(article, i) in articles">
             <v-flex xs12 sm6 md6 :key="i" px-3 py-3 class="article-cards">
-              <v-card class="mx-auto pa-2" height="140" :to="'/article/'+article.id">
+              <v-card class="mx-auto pa-2" height="200" :to="'/article/'+article.id">
                 <v-card height="40" flat>
                   <v-layout align-center justify-center fill-height text-center row wrap pa-0 ma-0>
                     <v-flex>
@@ -33,6 +33,11 @@
                     </v-flex>
                   </v-layout>
                 </v-card>
+                <v-card height="60" flat>
+                  <v-layout align-center justify-center fill-height text-center row wrap pa-3 ma-0>
+                    <div class="article-desc">{{article.content}}</div>
+                  </v-layout>
+                </v-card>
                 <v-card height="40" flat>
                   <v-layout align-center justify-center fill-height text-center row wrap pa-0 ma-0>
                     <v-flex>
@@ -53,6 +58,19 @@
 </template>
 
 <style>
+.article-desc {
+  overflow: hidden;
+  -webkit-line-clamp: 2;
+  line-clamp: 2;
+  -webkit-box-orient: vertical;
+  box-orient: vertical;
+  display: -webkit-box;
+  display: box;
+  word-break: break-all;
+  text-overflow: ellipsis;
+  color: grey;
+}
+
 .article-cards {
   font-family: "Roboto Condensed", sans-serif;
   font-size: 14px;

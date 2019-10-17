bac45b2cda27de01101eacd8c9bbb420ad23302e
hatlonely <hatlonely@gmail.com>
2019-10-18 04:48:49 +0800

    update

--- src/api/account.ts
+++ src/api/account.ts
@@ -1,9 +1,10 @@
 import axios from 'axios';
+
 import config from '../configs';
 
 export default {
     getAccount(token: string, callback: any, fallback: any) {
-        axios.get(config.api.account + '/account/' + token, {
+        axios.get(config.api.account + '/account/token/' + token, {
             withCredentials: true,
         }).then(callback).catch(fallback);
     },

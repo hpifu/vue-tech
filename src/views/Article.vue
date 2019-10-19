cdcad3d652d5f72ef624a89e82bb380f8a736f01
hatlonely <hatlonely@gmail.com>
2019-10-19 18:31:33 +0800

    update

---
+++ src/views/Article.vue
@@ -0,0 +1,114 @@
+<template>
+  <v-flex xs11 sm10 md8 lg6>
+    <v-card :loading="loading" flat pa-0 ma-0 class="fafafa-card">
+      <v-layout justify-center fill-height text-center row wrap ma-0 pa-0>
+        <v-flex md12 mt-5>
+          <h2>{{title}}</h2>
+        </v-flex>
+
+        <v-flex md12 mt-5>
+          <avatar
+            disabled
+            :ifcond="avatar"
+            :src="$config.api.cloud + '/avatar/' + authorID + '?name=' + avatar"
+          />
+          <span
+            class="grey--text font-weight-bold"
+          >{{author}}&nbsp;&nbsp;{{ctime ? new Date().toLocaleString() : ''}}</span>
+          <v-btn
+            v-if="this.$store.state.account.isSignedIn && this.authorID === this.$store.state.account.id"
+            class="ma-2"
+            outlined
+            x-small
+            fab
+            color="indigo"
+            :to="'/edit/' + this.$route.params.id"
+          >
+            <v-icon>mdi-pencil</v-icon>
+          </v-btn>
+        </v-flex>
+        <v-flex md12>
+          <template v-for="(tag, i) in tags">
+            <v-chip outlined color="green" class="mx-2 my-2" :key="i">#{{tag}}</v-chip>
+          </template>
+        </v-flex>
+        <div class="markdown-body">
+          <div v-html="content" class="text-left mt-3"></div>
+        </div>
+      </v-layout>
+    </v-card>
+  </v-flex>
+</template>
+
+<style>
+.fafafa-card.theme--light.v-card {
+  background-color: #fafafa;
+}
+@import url("../assets/css/md.css");
+@import url("https://cdnjs.cloudflare.com/ajax/libs/highlight.js/9.15.10/styles/github-gist.min.css");
+</style>
+
+<script lang="ts">
+import api from '@/api';
+import marked from 'marked';
+import hljs from '@/assets/ts/hljs';
+import { Component, Prop, Vue, Provide } from 'vue-property-decorator';
+import Avatar from '@/components/Avatar.vue';
+import hrenderer from '@/assets/ts/hrenderer';
+
+@Component({
+  components: {
+    Avatar,
+  },
+})
+export default class Article extends Vue {
+  @Provide() private title: string = '';
+  @Provide() private author: string = '';
+  @Provide() private authorID: number = 0;
+  @Provide() private content: string = '';
+  @Provide() private utime: string = '';
+  @Provide() private ctime: string = '';
+  @Provide() private tags: string = '';
+  @Provide() private avatar: string = '';
+  @Provide() private loading: boolean = false;
+
+  public beforeMount() {
+    marked.setOptions({
+      highlight(code: any, lang: any) {
+        try {
+          return hljs.highlight(lang, code).value;
+        } catch (err) {
+          return hljs.highlightAuto(code).value;
+        }
+      },
+    });
+
+    this.loading = true;
+    api.tech.getArticle(
+      this.$route.params.id,
+      (res: any) => {
+        this.loading = false;
+        if (res.status === 204) {
+          this.content = '204 NO CONTENT 没有该页面';
+        }
+        if (res.status === 200) {
+          this.title = res.data.title;
+          this.authorID = res.data.authorID;
+          this.author = res.data.author;
+          this.utime = res.data.utime;
+          this.ctime = res.data.ctime;
+          this.tags = res.data.tags;
+          this.avatar = res.data.avatar;
+          this.content = marked(res.data.content, {
+            renderer: hrenderer(this.authorID),
+          });
+        }
+      },
+      (err: any) => {
+        this.loading = false;
+        // console.log(err);
+      },
+    );
+  }
+}
+</script>
---
+++ src/views/Articles.vue
@@ -0,0 +1,106 @@
+<template>
+  <v-flex xs11 sm10 md8 lg6>
+    <v-layout align-center justify-center fill-height text-center row wrap ma-0 pa-0>
+      <div
+        v-infinite-scroll="loadMore"
+        infinite-scroll-disabled="$store.state.articles.busy"
+        infinite-scroll-distance="10"
+      >
+        <v-layout align-center justify-center fill-height text-center row wrap ma-0 pa-0>
+          <template v-for="(article, i) in $store.state.articles.articles">
+            <v-flex xs12 sm12 md6 :key="i" px-3 py-3 class="article-cards">
+              <v-card class="mx-auto pa-2" height="200" :to="'/article/'+article.id">
+                <v-card height="40" flat>
+                  <v-layout align-center justify-center fill-height text-center row wrap pa-0 ma-0>
+                    <v-flex>
+                      <h2 class="ma-0 pa-0">{{article.title}}</h2>
+                    </v-flex>
+                  </v-layout>
+                </v-card>
+                <v-card height="40" flat>
+                  <v-layout align-center justify-center fill-height text-center row wrap pa-0 ma-0>
+                    <v-flex>
+                      <template v-for="(tag, i) in article.tags">
+                        <v-chip
+                          outlined
+                          small
+                          color="green"
+                          class="mx-2 my-0"
+                          :key="i"
+                          v-if="i < 3"
+                        >#{{tag}}</v-chip>
+                      </template>
+                    </v-flex>
+                  </v-layout>
+                </v-card>
+                <v-card height="60" flat>
+                  <v-layout align-center justify-center fill-height text-center row wrap pa-3 ma-0>
+                    <div class="article-desc">{{article.content}}</div>
+                  </v-layout>
+                </v-card>
+                <v-card height="40" flat>
+                  <v-layout align-center justify-center fill-height text-center row wrap pa-0 ma-0>
+                    <v-flex>
+                      <p class="ma-0 pa-0">
+                        <avatar
+                          disabled
+                          :ifcond="article.avatar"
+                          :src="$config.api.cloud + '/avatar/' + article.authorID + '?name=' + article.avatar"
+                        />
+                        {{article.author}}
+                      </p>
+                    </v-flex>
+                    <v-flex>
+                      <p class="ma-0 pa-0">{{new Date(article.ctime).toLocaleString()}}</p>
+                    </v-flex>
+                  </v-layout>
+                </v-card>
+              </v-card>
+            </v-flex>
+          </template>
+        </v-layout>
+      </div>
+    </v-layout>
+  </v-flex>
+</template>
+
+<style>
+.article-desc {
+  overflow: hidden;
+  -webkit-line-clamp: 2;
+  line-clamp: 2;
+  -webkit-box-orient: vertical;
+  box-orient: vertical;
+  display: -webkit-box;
+  display: box;
+  word-break: break-all;
+  text-overflow: ellipsis;
+  color: grey;
+}
+
+.article-cards {
+  font-family: "Roboto Condensed", sans-serif;
+  font-size: 14px;
+  color: #333333;
+}
+
+.article-cards h2 {
+  font-size: 16px;
+}
+</style>
+
+<script lang="ts">
+import { Component, Prop, Vue, Provide } from 'vue-property-decorator';
+import Avatar from '@/components/Avatar.vue';
+
+@Component({
+  components: {
+    Avatar,
+  },
+})
+export default class Articles extends Vue {
+  public loadMore() {
+    this.$store.dispatch('articles/loadMore');
+  }
+}
+</script>
